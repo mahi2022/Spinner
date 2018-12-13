31c6b85fbcee1ba436a890fa7c17264c0776df1f
MAHI <maheshaddanki2220@gmail.com>
2018-12-13 09:08:30 +0530

    Initial commit

---
+++ app/src/main/java/com/mahi/spinner/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.mahi.spinner
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+mp.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {}
+    override fun onNothingSelected(parent: AdapterView<*>?) {}
+
+}
+
+
+    }
+}
